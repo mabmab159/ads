94452714026dda1cd8c9556baf02a030cc997dc7
Miguel Berrio <53589014+mabmab159@users.noreply.github.com>
2022-06-16 04:01:20 -0500

    Update plantilla.blade.php

--- resources/views/plantillas/plantilla.blade.php
+++ resources/views/plantillas/plantilla.blade.php
@@ -6,7 +6,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
