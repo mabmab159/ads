43a61a8fbc89eb1640cc21314f0c1b9ddcf7db87
MabMab^^ <miguelberriohotmail.com>
2022-06-21 11:25:05 -0500

    Agregando middlewares

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,30 @@ Route::post("/", [login::class, "login"])->name("validarUsuario");
 
 Route::get("/inicio", function () {
     return view("inicio")->with("libros", libros::paginate(25));
-})->name("inicio");
+})->name("inicio")->middleware("verificar");
 
-Route::post("/inicio", [librosController::class, "filtrarLibros"])->name("filtrarLibros");
+Route::post("/inicio", [librosController::class, "filtrarLibros"])->name("filtrarLibros")->middleware("verificar");
 
-Route::get("/prestamos", [prestamosController::class, "listarPrestamos"])->name("listarPrestamos");
+Route::get("/prestamos", [prestamosController::class, "listarPrestamos"])->name("listarPrestamos")->middleware("verificar");
 
-Route::post("/devolverLibro/{libros}", [librosController::class, "devolverLibro"])->name("devolverLibro");
+Route::post("/devolverLibro/{libros}", [librosController::class, "devolverLibro"])->name("devolverLibro")->middleware("verificar");
 
-Route::post("/prestamos", [librosController::class, "filtrarLibrosPrestamos"])->name("filtrarLibrosPrestamos");
+Route::post("/prestamos", [librosController::class, "filtrarLibrosPrestamos"])->name("filtrarLibrosPrestamos")->middleware("verificar");
 
 Route::get("/logout", [login::class, "logout"])->name("logout");
 
-Route::get("/registrarPrestamo", [prestamosController::class, "formularioPrestamos"])->name("formularioPrestamos");
+Route::get("/registrarPrestamo", [prestamosController::class, "formularioPrestamos"])->name("formularioPrestamos")->middleware("verificar");
 
-Route::post("/registrarPrestamo", [prestamosController::class, "registrarPrestamo"])->name("registrarPrestamo");
+Route::post("/registrarPrestamo", [prestamosController::class, "registrarPrestamo"])->name("registrarPrestamo")->middleware("verificar");
 
-Route::get("/validarISBN", [librosController::class, "formularioLibroISBN"])->name("formularioLibroISBN");
+Route::get("/validarISBN", [librosController::class, "formularioLibroISBN"])->name("formularioLibroISBN")->middleware("verificar");
 
-Route::get("/validarEliminarISBN", [librosController::class, "formularioEliminarLibroISBN"])->name("formularioEliminarLibroISBN");
+Route::get("/validarEliminarISBN", [librosController::class, "formularioEliminarLibroISBN"])->name("formularioEliminarLibroISBN")->middleware("verificar");
 
-Route::get("/registrarLibro", [librosController::class, "formularioLibro"])->name("formularioLibro");
+Route::get("/registrarLibro", [librosController::class, "formularioLibro"])->name("formularioLibro")->middleware("verificar");
 
-Route::post("/registrarLibro", [librosController::class, "registrarLibro"])->name("registrarLibro");
+Route::post("/registrarLibro", [librosController::class, "registrarLibro"])->name("registrarLibro")->middleware("verificar");
 
-Route::get("/eliminarLibro", [librosController::class, "formularioEliminarLibro"])->name("formularioEliminarLibro");
+Route::get("/eliminarLibro", [librosController::class, "formularioEliminarLibro"])->name("formularioEliminarLibro")->middleware("verificar");
 
-Route::post("/eliminarLibro", [librosController::class, "eliminarLibro"])->name("eliminarLibro");
+Route::post("/eliminarLibro", [librosController::class, "eliminarLibro"])->name("eliminarLibro")->middleware("verificar");
