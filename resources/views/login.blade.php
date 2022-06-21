a32cca465dd2779da9bd1501b741ff3392d2deee
MabMab^^ <miguelberriohotmail.com>
2022-06-21 13:44:07 -0500

    Cambiando el password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                       d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
             </svg>
-            <input name="password" class="w-full border-b-2 border-b-emerald-500">
+            <input name="password" type="password" class="w-full border-b-2 border-b-emerald-500">
         </div>
         <button type="submit"
                 class="bg-gradient-to-r from-emerald-500 to-emerald-600 text-white w-full py-2 rounded-full font-bold">
